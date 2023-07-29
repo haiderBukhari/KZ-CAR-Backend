9e6a4154dae57c35d20c0076c35ca4464e6f4f18
haiderBukhari <apshaiderbukhari786@gmail.com>
2023-07-29 20:15:28 +0500

    first commit1

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 import express from "express";
 import jsonServer from "json-server";
 import auth from "json-server-auth";
+import cors from "cors";
 
 const server = express();
 
 server.use(cors());
 
-const router = jsonServer.router('./data/db.json');
+const router = jsonServer.router('./Data/db.json');
 server.use('/api', router);
 server.db = router.db
 
 const middlewares = jsonServer.defaults()
 
-
 server.use(auth)
 server.use(middlewares)
 server.use(router)
-
-server.listen(8000);
+server.listen(9000);
